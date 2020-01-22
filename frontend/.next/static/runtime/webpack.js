cc5696fe6291276d451cc95e5724e08a60b7a48f
Oana Donose <oana.donose@mmtdigital.co.uk>
2020-01-22 10:55:20 +0000

    added signout

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "45451f9fc9642472e53d";
+/******/ 	var hotCurrentHash = "33fd65506a0f4307af6e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
