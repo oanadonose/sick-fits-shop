02d50cc99c17c497b7eb3eca59f5f8a58dd5e438
Oana Donose <oana.donose@mmtdigital.co.uk>
2020-01-29 12:16:35 +0000

    finished search

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7b7047de63380e5a4a71";
+/******/ 	var hotCurrentHash = "06af89d41eded3d1c8a6";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import Router from "next/router";
 import NProgress from "nprogress";
 import Cart from "./Cart";
+import Search from "./Search";
 
 Router.onRouteChangeStart = () => {
   console.log("onRouteChangeStart triggered");
@@ -70,7 +71,7 @@ const Header = () => (
     </div>
 
     <div className="sub-bar">
-      <p>Search</p>
+      <Search />
     </div>
     <Cart />
   </StyledHeader>
